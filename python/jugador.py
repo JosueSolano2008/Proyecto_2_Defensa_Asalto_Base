db15f06fae135d483fd8116a30aed5472ff751f4
JosueSolano2008 <foquitoxview@gmail.com>
2026-06-16 12:52:51 -0600

    Registro de jugadores

--- python/jugador.py
+++ python/jugador.py
@@ -9,3 +9,32 @@ class Jugador:
         self.contrasenia = contrasenia
         self.victorias_defensor = 0
         self.victorias_atacante = 0
+
+    #archivo
+    def cargar_jugadores (self):
+        if not os.path.exists(archivo_jugadores):
+            return {"jugadores":{}}
+        with open(archivo_jugadores, "r") as f:
+            return json.load(f)
+
+    def guardar_jugadores(self, datos):
+        with open(archivo_jugadores, "w") as f:
+            return json.dump(datos,f,indent=4)
+
+    #Registro
+    def registrar(self):
+        datos=self.cargar_jugadores()
+
+        if self.nombre_usuario in datos ["jugadores"]:
+            return False,"El usuario ya existe"
+        datos["Jugadores"][self.nombre_usuario]={
+            "contraseña" : self.contraseña,
+            "victorias_defensor": self.victorias_defensor,
+            "victorias_atacante": self.victorias_atacante
+        }
+        self.guardar_jugadores(datos)
+        return True, "Usuario registrado correctamente"
+    
+    #Login
+    def iniciar_sesion(self, nombre_usuario,contrasenia):
+        datos = self.cargar_jugadores
